957ab2f5a4d03ef002b815d5fba6a6acdd5361c8
Lee Hangsen <hangsen.lee@hexainitiative.com>
2021-02-09 17:43:22 +0800

    Added a HTML converter to RecyclerAdapter.kt to convert encoded strings into a displayable format

--- app/src/main/java/com/example/opentriviadbdemoapp/ui/adapter/RecyclerAdapter.kt
+++ app/src/main/java/com/example/opentriviadbdemoapp/ui/adapter/RecyclerAdapter.kt
@@ -2,13 +2,14 @@ package com.example.opentriviadbdemoapp.ui.adapter
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.text.HtmlCompat
 import androidx.recyclerview.widget.RecyclerView
+import com.example.opentriviadbdemoapp.data.model.QuizQuestion
 import com.example.opentriviadbdemoapp.databinding.ItemBrowseCardviewBinding
 
-class RecyclerAdapter(
-    private var list: ArrayList<String> = ArrayList()
+class RecyclerAdapter() : RecyclerView.Adapter<RecyclerAdapter.ViewHolder>() {
 
-) : RecyclerView.Adapter<RecyclerAdapter.ViewHolder>() {
+    private var list = emptyList<QuizQuestion>()
 
     inner class ViewHolder(val binding: ItemBrowseCardviewBinding) :
         RecyclerView.ViewHolder(binding.root) {
@@ -25,13 +26,21 @@ class RecyclerAdapter(
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.binding.tvQuestion.text = list[position]
-        holder.binding.chDifficulty.text = list[position]
-        holder.binding.chType.text = list[position]
+        val convertText =
+            HtmlCompat.fromHtml(list[position].quizQuestion, HtmlCompat.FROM_HTML_MODE_LEGACY)
+
+        holder.binding.tvQuestion.text = convertText.toString()
+        holder.binding.chDifficulty.text = list[position].quizDifficulty
+        holder.binding.chType.text = list[position].quizType
 
 //        holder.itemView.setOnClickListener { v ->
 //            v.context as AppCompatActivity
 //        }
+    }
+
+    fun setData(newList: List<QuizQuestion>) {
+        list = newList
+        notifyDataSetChanged()
     }
 
     override fun getItemCount(): Int {
